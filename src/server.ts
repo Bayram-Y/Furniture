7d53523035388278f55aec1958b3194949614aac
Bayram-Y <bayramyaxiyaev7@gmail.com>
2025-04-09 16:39:44 +0900

    feat: prepare for production

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,17 @@
 import dotenv from "dotenv";
-dotenv.config();
+dotenv.config({
+  path: process.env.NODE_ENV === "production" ? ".env.production" : ".env",
+});
 import server from "./app";
-
 import mongoose from "mongoose";
+
+
 
 mongoose
   .connect(process.env.MONGO_URL as string, {})
   .then((data) => {
     console.log("MongoDB connected successfully");
-    const PORT = process.env.PORT ?? 3004;
+    const PORT = process.env.PORT ?? 3003;
     server.listen(PORT, function () {
       console.log(`The server is running successfully on port: ${PORT}`);
       console.info(`Admin project on http://localhost:${PORT}/admin \n`);
